9e34d1763a4675a1c8ddaea8b818e97fa81544d4
Gabriel Botareli <gbotareli@gmail.com>
2021-11-11 07:56:34 -0300

    validador se a categoria de carro já existe.

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -15,7 +15,6 @@ class CategoriesRepository {
 
     create({ description, name }: ICreateCategoryDTO): void {
         const category = new Category();
-
         Object.assign(category, {
             name,
             description,
@@ -23,6 +22,15 @@ class CategoriesRepository {
         });
 
         this.categories.push(category);
+    }
+
+    list(): Category[] {
+        return this.categories;
+    }
+
+    findByName(name: string): Category {
+        const category = this.categories.find(category => category.name === name)
+        return category
     }
 }
 
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -6,9 +6,19 @@ const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
+    const categoryAlreadyExists = categoriesRepository.findByName(name)
+
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category already existis." })
+    }
     categoriesRepository.create({ name, description });
 
     return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+    return response.status(200).json(all);
 });
 
 export { categoriesRoutes };
