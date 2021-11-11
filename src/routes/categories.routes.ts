fc9b0e7ed990b1628e81079922fc3aff3b163d04
Gabriel Botareli <gbotareli@gmail.com>
2021-11-10 21:53:17 -0300

    criação de routes e repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,12 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories = []
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
-    const category = {
-        id: uuidv4(),
-        name,
-        description
-    };
-    categories.push(category);
+    categoriesRepository.create({ name, description });
 
     return response.status(201).send();
 });
